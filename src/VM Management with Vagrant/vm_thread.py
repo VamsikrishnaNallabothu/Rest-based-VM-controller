11e85ac68f83193b4d3f51655e718f0804003e61
Vamsi krishna Nallabothu <VamsikrishnaNallabothu@users.noreply.github.com>
2016-05-20 12:52:49 -0700

    Final Commit

---
+++ src/VM Management with Vagrant/vm_thread.py
@@ -0,0 +1,14 @@
+
+import threading
+
+from vm_manager import Vm_manger_vagrant
+
+def create_vm(config):
+    vm = Vm_manger_vagrant()
+    vm.create_vm(config)
+
+
+def run_thread(config):
+    t = threading.Thread(target=create_vm, args=(config,))
+    t.start()
+
